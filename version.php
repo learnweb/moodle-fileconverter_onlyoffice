cee36ac5774bd27fcf53fb6d1ad8947cbcf605bc
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2019-12-22 18:37:01 +0100

    prepare first stable release

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'fileconverter_onlyoffice';
 $plugin->release = '3.8-r1';
-$plugin->version = 2019112703;
+$plugin->version = 2019122200;
 $plugin->requires = 2019052000;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_STABLE;
