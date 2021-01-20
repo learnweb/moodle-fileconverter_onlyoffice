85618f688eaf88c6439d62b3df37f59f7d865651
timotejj <timotej.jazbec1@guest.arnes.si>
2021-01-20 11:59:02 +0100

    add newline

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'fileconverter_onlyoffice';
 $plugin->release = '3.8-r1';
-$plugin->version = 2019122200;
+$plugin->version = 2021012000;
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_STABLE;
