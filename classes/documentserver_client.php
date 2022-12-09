4657fc84015e438e4a76fde9d168d40250ce3397
Justus Dieckmann <justusdieckmann@wwu.de>
2022-12-09 19:48:28 +0100

    Add support for server with secret

--- classes/documentserver_client.php
+++ classes/documentserver_client.php
@@ -25,6 +25,7 @@ namespace fileconverter_onlyoffice;
 
 use coding_exception;
 use curl;
+use Firebase\JWT\JWT;
 
 defined('MOODLE_INTERNAL') || die();
 
@@ -48,20 +49,30 @@ class documentserver_client {
     private $documentserverhost;
 
     /**
+     * @var string|false optional secret for communication.
+     */
+    private $secret;
+
+    /**
      * Initialise the client.
      * @param string $documentserverhost Private OnlyOfice document server URL
      */
     public function __construct(string $documentserverhost) {
         $this->documentserverhost = rtrim($documentserverhost, '/');
         $this->curl = new \curl();
+        $this->secret = get_config('fileconverter_onlyoffice', 'secret');
     }
 
     public function request_conversion($params) {
         $endpoint = $this->documentserverhost . '/ConvertService.ashx';
-        $callargs = json_encode($params);
+        if ($this->secret) {
+            $body = json_encode(['token' => JWT::encode($params, $this->secret, 'HS256')]);
+        } else {
+            $body = json_encode($params);
+        }
         $this->curl->setHeader('Content-type: application/json');
         $this->curl->setHeader('Accept: application/json');
-        $response = $this->curl->post($endpoint, $callargs);
+        $response = $this->curl->post($endpoint, $body);
 
         if ($this->curl->errno != 0) {
             throw new coding_exception($this->curl->error, $this->curl->errno);
@@ -70,7 +81,18 @@ class documentserver_client {
         $json = json_decode($response);
 
         if (!empty($json->error)) {
-            throw new coding_exception($json->error->code . ': ' . $json->error->message . '. Response was: '.$response);
+            if (is_int($json->error)) {
+                if ($json->error == -8) {
+                    if ($this->secret) {
+                        throw new coding_exception("Invalid secret configured!");
+                    } else {
+                        throw new coding_exception("The server requires a secret. Please set it in the plugin settings.");
+                    }
+                }
+                throw new coding_exception("Error $json->error, see https://api.onlyoffice.com/editors/conversionapi#error-codes . Response was " . $response);
+            } else {
+                throw new coding_exception($json->error->code . ': ' . $json->error->message . '. Response was: '.$response);
+            }
         }
         return $json;
     }
